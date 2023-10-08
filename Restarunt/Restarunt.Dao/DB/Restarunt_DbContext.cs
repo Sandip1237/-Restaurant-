489331f65f1cd12e7dc2235a5f8ec74ccee290e6
Sandip1237 <sandipkorake25@gmail.com>
2023-10-08 14:04:41 +0530

    -Initial Commit -Implemented base architecture -Created Login Web API

---
+++ Restarunt/Restarunt.Dao/DB/Restarunt_DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restarunt.common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restarunt.Dao.DB
+{
+    public class Restarunt_DbContext:DbContext
+    {
+        public DbSet<User> tbl_User { get; set; }
+
+        public DbSet<LoginDetails> tbl_Login_Details { get; set; }
+        public Restarunt_DbContext(DbContextOptions<Restarunt_DbContext> options) : base(options) { 
+        
+        }
+    }
+}
